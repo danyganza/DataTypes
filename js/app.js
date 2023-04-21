c4d7a3c71d812341ff25c9d8ea71a7c84bbf3437
dannyganza <kingdanny@Dannys-Air.hitronhub.home>
2023-04-21 10:07:06 -0600

    added numbers

--- js/app.js
+++ js/app.js
@@ -3,3 +3,9 @@ let user_age = `21`;
 
 console.log(`hello, ${username}'`);
 console.log(`you are ${username}`)
+
+// adding numbers 
+let max_volume = 100;
+let current_volume= 50;
+
+let user_score = 70/50*100;
